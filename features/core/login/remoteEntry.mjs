04cdbb962be5ba0f998392b51876f4955777ae9a
Oscar Mora Quind <oscar.mora@quind.io>
2025-01-22 16:27:36 -0500

    first commit

--- features/core/login/remoteEntry.mjs
+++ features/core/login/remoteEntry.mjs
@@ -5227,7 +5227,7 @@ if(!__webpack_require__.federation.instance){
 /******/ 	// This function allow to reference async chunks
 /******/ 	__webpack_require__.u = (chunkId) => {
 /******/ 		// return url for filenames based on template
-/******/ 		return "" + ({"76":"common","352":"__federation_expose_confirm","364":"__federation_expose_signup","470":"__federation_expose_signin","847":"__federation_expose_login"}[chunkId] || chunkId) + "." + {"40":"942b3f44f4bf7587","64":"46fe9cd95fea2c9d","76":"075972697c89e142","123":"21c289d0075fac59","214":"0f718594e208e8a8","352":"8250c90c5500692f","356":"8683c209ccbea732","364":"532ca779083a79ca","365":"7a575788fff2d45b","367":"461da326e1554f4b","467":"12c88acc3a786ea1","470":"00a377e29aa7e1d5","487":"3ff0199ce92fd0d7","609":"25be961e861db21d","630":"d07dd4f0e63d0c4b","847":"06086bc3fbc0d1c8","872":"238ba2c2b8700d90"}[chunkId] + ".js";
+/******/ 		return "" + ({"76":"common","352":"__federation_expose_confirm","364":"__federation_expose_signup","470":"__federation_expose_signin","847":"__federation_expose_login"}[chunkId] || chunkId) + "." + {"40":"942b3f44f4bf7587","64":"46fe9cd95fea2c9d","76":"075972697c89e142","123":"21c289d0075fac59","214":"0f718594e208e8a8","352":"8250c90c5500692f","356":"8683c209ccbea732","364":"532ca779083a79ca","365":"7a575788fff2d45b","367":"461da326e1554f4b","467":"12c88acc3a786ea1","470":"62530fa84a8d5107","487":"3ff0199ce92fd0d7","609":"25be961e861db21d","630":"d07dd4f0e63d0c4b","847":"06086bc3fbc0d1c8","872":"238ba2c2b8700d90"}[chunkId] + ".js";
 /******/ 	};
 /******/ })();
 /******/ 
